7f2473abbc58a99e0ec1f33fa15d0ac928e87b3c
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-18 16:52:48 +0000

    Add initialize() and save() methods to Tag class and test data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require('pry')
 
 require_relative('../models/user.rb')
+require_relative('../models/tag.rb')
+require_relative('../models/transaction.rb')
 
 User.delete_all()
 
@@ -24,6 +26,27 @@ user1.name = "John"
 user1.net_monthly_income = 1_790.88
 user1.monthly_budget = 1_000.00
 user1.update()
+
+tag1 = Tag.new({
+  "category" => "food"
+  })
+tag1.save()
+tag2 = Tag.new({
+  "category" => "clothing"
+  })
+tag2.save()
+tag3 = Tag.new({
+  "category" => "entertainment"
+  })
+tag3.save()
+tag4 = Tag.new({
+  "category" => "travel"
+  })
+tag4.save()
+tag5 = Tag.new({
+  "category" => "groceries"
+  })
+tag5.save()
 
 binding.pry
 nil
---
+++ models/tag.rb
@@ -0,0 +1,20 @@
+require_relative('../db/sql_runner')
+
+class Tag
+
+  attr_reader :id
+  attr_accessor :category
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @category = options['category']
+  end
+
+  def save()
+    sql = "INSERT INTO tags (category) VALUES ($1) RETURNING id"
+    values = [@category]
+    results = SqlRunner.run( sql, values )
+    @id = results.first()['id'].to_i
+  end
+
+end
