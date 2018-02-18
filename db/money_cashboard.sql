681795b042599011ff3a9d90558f013527a2776b
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-18 13:09:34 +0000

    Add pseudocode to user.rb to show all required methods

--- db/money_cashboard.sql
+++ db/money_cashboard.sql
@@ -4,20 +4,20 @@ DROP TABLE users;
 
 CREATE TABLE tags (
   id SERIAL PRIMARY KEY,
-  category VARCHAR(100)
+  category VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  net_monthly_income MONEY,
-  monthly_budget MONEY
+  name VARCHAR(100) NOT NULL,
+  net_monthly_income MONEY NOT NULL,
+  monthly_budget MONEY NOT NULL
 );
 
 CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
   merchant_name VARCHAR(100),
-  amount MONEY,
+  amount MONEY NOT NULL,
   transaction_date DATE,
   tag_id INT REFERENCES tags(id)
 );
--- models/user.rb
+++ models/user.rb
@@ -9,4 +9,56 @@ class User
     @monthly_budget = options['monthly_budget'].to_f
   end
 
+  # add_user class method
+    # create query string to insert/create user
+    # create parameters to pass into the query string
+    # execute the query using the sql_runner.rb run method
+    # return the @id from the new db entry
+
+  # update_user instance method
+    # create query string to update user
+    # create parameters to pass into the query string
+    # execute the query using the sql_runner.rb run method
+
+  # return all users class method
+    # create query string to return all users
+    # create parameters to pass into the query string
+    # return array of hases by executing the query using the sql_runner.rb run method
+    # use the map() method to convert to an array of User objects
+
+  # delete_user instance method
+    # create query string to delete user
+    # create parameters to pass into the query string
+    # execute the query using the sql_runner.rb run method
+
+  # return all_transactions instance method
+    # create query string to return all transactions
+    # create parameters to pass into the query string
+    # return array of hases by executing the query using the sql_runner.rb run method
+    # use the map() method to convert to an array of transaction objects
+
+  # return all_transactions_by_month instance method
+    # create query string to return all transactions by month
+    # create parameters to pass into the query string
+    # return array of hases by executing the query using the sql_runner.rb run method
+    # use the map() method to convert to an array of transaction objects
+
+  # return all_transactions_by_tag instance method
+    # create query string to return all transactions by tag
+    # create parameters to pass into the query string
+    # return array of hases by executing the query using the sql_runner.rb run method
+    # use the map() method to convert to an array of transaction objects
+
+  # return total_spend instance method
+    # call all_transactions method
+    # sum() all transactions
+    # return sum
+
+  # return total_spend_by_month instance method
+    # call all_transactions_by_month
+    # sum() all transactions
+    # return sum
+
+
+
 end
