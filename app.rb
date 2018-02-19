9c0352494f865c43e840e4a5589c806e81483374
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-19 09:47:11 +0000

    Add layout.erb, index.erb and update app.rb to display total_spent

---
+++ app.rb
@@ -0,0 +1,13 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/user_controller')
+require_relative( './models/user.rb' )
+require_relative( './models/transaction.rb' )
+require_relative( './models/tag.rb' )
+
+
+get '/' do
+  user = User.find(1)
+  @total_spent = user.total_spent()
+  erb( :index )
+end
--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -3,5 +3,5 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/user.rb' )
 
 get '/users' do
-  return "This is a test string for /users index"
+  erb(:"users/index")
 end
